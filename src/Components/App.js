d8122e08af3c141c454bd83f8213b3af7e455125
Blagoja95 <boris.blagojevicc@hotmail.com>
2022-05-14 18:15:55 +0200

    other products section partial

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../SASS/main.scss";
 import Header from "../Sections/Header/Header";
 import Moment from "../Sections/Moment/Moment";
+import Other from "../Sections/Other/Other";
 import Specification from "../Sections/Specification/Specification";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Specification />
       <Moment />
+      <Other />
     </main>
   );
 };
---
+++ src/Sections/Other/Other.js
@@ -0,0 +1,18 @@
+const Other = () => {
+  return (
+    <section className="other">
+      <div className="other__inner">
+        <div className="other__content">
+          <h1 className="title other__title">Povezani proizvodi</h1>
+          <p className="paragraph other__paragraph">
+            Large-size earmuffs that could cover the whole ears, paired with
+            thick, soft earmuffscan make it comfortable to immerse in music for
+            a long time.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Other;
